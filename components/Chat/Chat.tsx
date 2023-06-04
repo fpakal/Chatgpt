2a28a0dde2008f09e682a0a0c0c51e121d6807b9
fpakal <129833941+fpakal@users.noreply.github.com>
2023-06-03 21:40:29 -0400

    Update Chat.tsx

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -351,28 +351,27 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
     <div className="relative flex-1 overflow-hidden bg-white dark:bg-[#343541]">
       {!(apiKey || serverSideApiKeyIsSet) ? (
         <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
-       <div className="text-center text-4xl font-bold text-black dark:text-white">
-  <h1>Welcome to ChatGPT1.org by Don Memon</h1>
-</div>
-<div className="text-center text-lg text-black dark:text-white">
-  <div className="mb-8">{`ChatGPT1.org is an open source clone of OpenAI's ChatGPT UI.`}</div>
-  <div className="mb-2 font-bold">
-    Important: ChatGPT1.org is 100% unaffiliated with OpenAI or any other Chatbot
-  </div>
-</div>
-<div className="text-center text-gray-500 dark:text-gray-400">
-  <div className="mb-2">
-    ChatGPT1 allows you to plug in your API key to use this UI with their API.
-  </div>
-</div>
-
+          <div className="text-center text-4xl font-bold text-black dark:text-white">
+            Welcome to ChatGPT1.org By Don Memon
+          </div>
+          <div className="text-center text-lg text-black dark:text-white">
+            <div className="mb-8">{`ChatGPT1.org is an open source clone of OpenAI's ChatGPT UI.`}</div>
+            <div className="mb-2 font-bold">
+              Important: ChatGPT1.org  is 100% unaffiliated with OpenAI.
+            </div>
+          </div>
+          <div className="text-center text-gray-500 dark:text-gray-400">
+            <div className="mb-2">
+              Chatbot UI allows you to plug in your API key to use this UI with
+              their API.
+            </div>
             <div className="mb-2">
               It is <span className="italic">only</span> used to communicate
               with their API.
             </div>
             <div className="mb-2">
               {t(
-                'Please set your OpenAI API key in the bottom left of the sidebar. For Mobile User click Arror on Top Left',
+                'Please set your OpenAI API key in the bottom left of the sidebar.',
               )}
             </div>
             <div>
