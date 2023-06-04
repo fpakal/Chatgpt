e1383da2529922a4a84fb78e0d176d024aa8faa7
fpakal <129833941+fpakal@users.noreply.github.com>
2023-06-03 20:34:51 -0400

    Update _document.tsx
    eror

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,12 +11,13 @@ export default function Document(props: Props) {
     props.__NEXT_DATA__.locale ?? i18nextConfig.i18n.defaultLocale;
   return (
     <Html lang={currentLocale}>
-      <Head>
+    <Head>
   <meta name="apple-mobile-web-app-capable" content="yes" />
-  <meta name="apple-mobile-web-app-title" content="ChatGPT1.org by Don Memon"></meta>
+  <meta name="apple-mobile-web-app-title" content="ChatGPT1.org by Don Memon" />
   <title>ChatGPT1.org by Don Memon</title>
-  <meta name="description" content="Experience the power of AI with our advanced chatbot. Leveraging the capabilities of GPT-3, our chatbot provides accurate responses and includes a search function for quick information retrieval. Ideal for anyone looking to interact with cutting-edge AI technology.">
+  <meta name="description" content="Experience the power of AI with our advanced chatbot. Leveraging the capabilities of GPT-3, our chatbot provides accurate responses and includes a search function for quick information retrieval. Ideal for anyone looking to interact with cutting-edge AI technology." />
 </Head>
+
 
       <body>
         <Main />
